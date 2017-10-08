8c4debb53e9cd70835c8ebed5fcfda4354684056
Jake T <jaketepper@gmail.com>
2017-10-08 18:36:34 -0400

    first commit

---
+++ src/main/java/com/arcsoftware/CTWebsite/CtWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.arcsoftware.CTWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtWebsiteApplication.class, args);
+	}
+}
